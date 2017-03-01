1e8fcaa45fe0453f5f62fa6b94527e21d40aa1e1
tpnguyen15 <tpnguy15@student.uia.no>
2017-03-01 16:28:59 +0100

    Oppgave 1a fixed

--- Oppgave1/ASCII/ascii.go
+++ Oppgave1/ASCII/ascii.go
@@ -1,6 +1,10 @@
 package ascii
 
-const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
+import (
+	"fmt"
+)
+
+const Ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
 	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
 	` !"#$%&'()*+,-./0123456789:;<=>?` +
 	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
@@ -16,8 +20,13 @@ const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 // 3F ? 111111
 // 40 @ 1000000
 // ...
-func IterateOverASCIIStringLiteral(sl string) {
-	// Kode for Oppgave 1a
+func IterateOverASCIIStringLiteral(ascii string) {
+
+for i := 0; i < len(ascii); i++ {
+fmt.Printf("%X %+q %b \n", ascii[i], ascii[i], ascii[i] )
+
+
+}
 }
 
 // Funksjonen skal generere en utskrift fra en sekvens av bytes,
@@ -28,4 +37,3 @@ func IterateOverASCIIStringLiteral(sl string) {
 // Funksjonen greetingASCII() returnerer en variabel av typen string,
 // som inneholder kun ASCII tegn (ikke utvidet ASCII).
 // Gjelder oppgave 1b
-func GreetingASCII() {}
