0458f0f815af18ddb1b5e222eb4530ae5f5efd06
osy16 <osmany16@student.uia.no>
2017-05-11 12:55:00 +0200

    Update main.go

--- Oppgave1(b)/main.go
+++ Oppgave1(b)/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
   "./ascii"
+  "fmt"
 )
 
 func main() {
-  ascii.GreetingASCII()
+  print := ascii.GreetingASCII()
+ 	fmt.Println("Print av return HEX:")
+ 	fmt.Println(print)
+
 }
